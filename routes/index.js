9c102b980ef324c2ab900c12fe6c629487a13608
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-03-09 19:12:05 +0000

    task 3 add users to db

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 /* routes module */
 const routes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+  app.post('/users', UsersController.postNew);
 };
 module.exports = routes;
