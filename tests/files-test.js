803275132cb65622bd675cc805a9aaecb2055eb5
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-03-17 01:38:47 +0000

    add tests for files

--- tests/files-test.js
+++ tests/files-test.js
@@ -3,6 +3,7 @@ import {
   describe,
   before,
   after,
+  beforeEach,
 } from 'mocha';
 import fs from 'fs';
 import { MongoClient } from 'mongodb';
@@ -18,6 +19,7 @@ const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
 let client;
 let db;
+let token;
 describe('test files', () => {
   /*
    * @before: hook to define database and insert
@@ -33,6 +35,11 @@ describe('test files', () => {
         await db.collection('users').insertOne({ email: 'bob@dylan.com', password: sha1('toto1234!') });
       }
     });
+  });
+
+  beforeEach(async () => {
+    const connectresponse = await request(app).get('/connect').set('Authorization', 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=').send();
+    token = connectresponse.body.token;
   });
 
   /*
@@ -53,8 +60,6 @@ describe('test files', () => {
    */
   it('post /files', async () => {
     const data = { name: 'myText.txt', type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
-    const connectresponse = await request(app).get('/connect').set('Authorization', 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=').send();
-    const { token } = connectresponse.body;
     const response = await request(app).post('/files').set('x-token', token).send(data);
     expect(response.statusCode).to.be.equal(201);
     expect(response.body.name).to.be.equal('myText.txt');
@@ -65,5 +70,39 @@ describe('test files', () => {
     } catch (err) {
       throw new Error(err);
     }
+  });
+
+  /*
+   * @test: to check if the file is created
+   * and a document is created when post
+   * request is made check { errors }
+   *
+   */
+  it('post /files', async () => {
+    let data;
+    let response;
+    // missing data
+    data = { name: 'myText.txt', type: 'file' };
+    response = await request(app).post('/files').set('x-token', token).send(data);
+    expect(response.statusCode).to.be.equal(400);
+    expect(response.body).to.deep.equal({ error: 'Missing data' });
+    // missing type
+    data = { name: 'myText.txt', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
+    response = await request(app).post('/files').set('x-token', token).send(data);
+    expect(response.statusCode).to.be.equal(400);
+    expect(response.body).to.deep.equal({ error: 'Missing type' });
+    // test non folder parent id passed error
+    data = { name: 'file.txt', type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
+    response = await request(app).post('/files').set('x-token', token).send(data);
+    const { id } = response.body;
+    data = {
+      name: 'file2.txt',
+      type: 'file',
+      data: 'SGVsbG8gV2Vic3RhY2shCg==',
+      parentId: id,
+    };
+    response = await request(app).post('/files').set('x-token', token).send(data);
+    expect(response.statusCode).to.be.equal(400);
+    expect(response.body).to.deep.equal({ error: 'Parent is not a folder' });
   });
 });
